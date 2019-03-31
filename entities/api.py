a84367bcc99fb0b2689c82bcb8cfcb7879322cea
ignalion <ignalion@gmail.com>
2019-03-31 05:03:58 +0300

    Added Rex entity

--- entities/api.py
+++ entities/api.py
@@ -1,6 +1,7 @@
 from core import engine
 from core.entity import Entity
 from entities.point import Point
+from entities.rex import Rex
 from lib.ipc import get_router
 from lib.ipc.gateways.pikagw import BlockingPikaGateway
 
@@ -11,12 +12,16 @@ class API(Entity):
         self.router = get_router('main', token='game', exchange=exchange,
                                  mq_host=url, gw_cls=BlockingPikaGateway)
         self.router.handlers.add_point = self.add_point
+        self.router.handlers.add_rex = self.add_rex
         self.router.handlers.entities = engine.ENTITIES
 
         await self.router.start()
 
     async def add_point(self, x=0, y=0):
         await engine.create_entity(Point, x=x, y=y)
+
+    async def add_rex(self, x=0, y=0):
+        await engine.create_entity(Rex, pos=(x, y))
 
     def tick(self):
         self.router.gw._process()
